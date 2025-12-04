c7900f16e72af4beb64e09f3e95ab8a3730ab4a8
TuanAnhCu2003 <tuananht008@gmail.com>
2025-12-04 23:52:39 +0700

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import CalendarApp from "./Components/CalendarApp"
+const App = () => {
+  return (
+    <div>
+      <CalendarApp />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/CalendarApp.jsx
@@ -0,0 +1,8 @@
+const CalendarApp = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default CalendarApp
